373f751e3f6e1aaebfa844372279cc2651f78234
DominiqueMakowski <dom.mak19@gmail.com>
2025-10-16 09:34:32 +0100

    rename variables

--- analysis/0_make_data_for_teaching.R
+++ analysis/0_make_data_for_teaching.R
@@ -4,7 +4,7 @@ df <- read.csv("../data/rawdata_participants.csv")
 dftask <- read.csv("../data/rawdata_task.csv")
 
 # Questionnaires scores
-df$BAIT_Total <- df |>
+df$BAIT <- df |>
   select(starts_with("BAIT_"), -BAIT_AttentionCheck, -BAIT_AI_Use, -BAIT_AI_Knowledge) |>
   # Reverse
   mutate(
@@ -22,13 +22,13 @@ df <- df |>
   pivot_longer(-Participant, names_to = "Item", values_to = "Response") |>
   separate(Item, into = c("MIST", "nature", "type", "item"), sep = "_") |>
   # filter(nature == "fake") |>
-  summarize(MIST_p_False = sum(Response <= 0, na.rm = TRUE) / n(), .by = "Participant") |>
+  summarize(MIST = sum(Response <= 0, na.rm = TRUE) / n(), .by = "Participant") |>
   left_join(df, by = join_by("Participant"))
 
 # Only select the first 10 items which are pseudo-profound bullshit
-df$BRS_Total <- rowMeans(df[, paste0("BRS_", 1:10)])
+df$BRS <- rowMeans(df[, paste0("BRS_", 1:10)])
 df$BRS_Control <- rowMeans(df[, paste0("BRS_", 11:20)])
-df$BRS_Sensitivity <- df$BRS_Total - df$BRS_Control
+df$BRS_Sensitivity <- df$BRS - df$BRS_Control
 
 
 # Attention checks
@@ -45,28 +45,28 @@ df <- dftask |>
     # Task_p_Real = sum(Reality >= 0) / n(),
     # Task_Confidence = mean(abs(Reality) / 100),
     # Task_FakeAttractiveness = mean(ifelse(Condition == "AI-Generated", Attractiveness, NA), na.rm = TRUE),
-    Task_SyntheticPenalty = mean(ifelse(Condition == "Photograph", Attractiveness, NA), na.rm = TRUE) - mean(ifelse(Condition == "AI-Generated", Attractiveness, NA), na.rm = TRUE),
+    AttractivenessBias = mean(ifelse(Condition == "Photograph", Attractiveness, NA), na.rm = TRUE) - mean(ifelse(Condition == "AI-Generated", Attractiveness, NA), na.rm = TRUE),
     .by = "Participant"
   ) |>
   left_join(df, by = join_by("Participant"))
 
 # hist(df$AttentionChecks)
 #
-parameters::parameters(lm(Task_SyntheticPenalty ~ MIST_p_False + BRS_Total, data = df))
-parameters::parameters(lm(Task_SyntheticPenalty ~ BAIT_Total + BRS_Total, data = df))
+parameters::parameters(lm(AttractivenessBias ~ MIST + BRS, data = df))
+parameters::parameters(lm(AttractivenessBias ~ BAIT + BRS, data = df))
 #
-# parameters::parameters(lm(Task_SyntheticPenalty ~ MIST_p_False + BAIT_Total, data = df))
-# parameters::parameters(lm(Task_SyntheticPenalty ~ BRS_Total + BAIT_Total, data = df))
-# parameters::parameters(lm(Task_SyntheticPenalty ~ BRS_Total + MIST_p_False + BAIT_Total, data = df))
+# parameters::parameters(lm(AttractivenessBias ~ MIST + BAIT, data = df))
+# parameters::parameters(lm(AttractivenessBias ~ BRS + BAIT, data = df))
+# parameters::parameters(lm(AttractivenessBias ~ BRS + MIST + BAIT, data = df))
 #
 #
-# plot(correlation::cor_test(df, "Task_SyntheticPenalty", "MIST_p_False"))
-# plot(correlation::cor_test(df, "Task_SyntheticPenalty", "BAIT_Total"))
+# plot(correlation::cor_test(df, "AttractivenessBias", "MIST"))
+# plot(correlation::cor_test(df, "AttractivenessBias", "BAIT"))
 #
-# parameters::parameters(lm(Task_SyntheticPenalty ~ BRS_Total + BAIT_Total, data = filter(df, Gender == "Man")))
-# parameters::parameters(lm(Task_SyntheticPenalty ~ MIST_p_True + BAIT_Total, data = filter(df, Gender == "Man")))
-# parameters::parameters(lm(Task_SyntheticPenalty ~ BRS_Total + BAIT_Total, data = filter(df, Gender == "Woman")))
-# parameters::parameters(lm(Task_SyntheticPenalty ~ MIST_p_True + BAIT_Total, data = filter(df, Gender == "Woman")))
+# parameters::parameters(lm(AttractivenessBias ~ BRS + BAIT, data = filter(df, Gender == "Man")))
+# parameters::parameters(lm(AttractivenessBias ~ MIST_p_True + BAIT, data = filter(df, Gender == "Man")))
+# parameters::parameters(lm(AttractivenessBias ~ BRS + BAIT, data = filter(df, Gender == "Woman")))
+# parameters::parameters(lm(AttractivenessBias ~ MIST_p_True + BAIT, data = filter(df, Gender == "Woman")))
 
 
 # Scramble demographics
@@ -77,11 +77,11 @@ df$Age <- sample(df$Age)
 
 # Save
 data <- df |>
-  select(Participant, Age, Gender, Task_SyntheticPenalty, BAIT_Total, MIST_p_False, BRS_Total, AttentionChecks) |>
+  select(Participant, Age, Gender, AttractivenessBias, BAIT, MIST, BRS, AttentionChecks) |>
   arrange(Participant)
 
 summary(data)
-# write.csv(data, "../data/DiscoveringStats2025.csv", row.names = FALSE)
+write.csv(data, "../data/DiscoveringStats2025.csv", row.names = FALSE)
 
 
 # data |>
